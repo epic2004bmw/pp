d7db474f1869556a94842a23332ed5205c828721
Bryce Wong <epic2004bmw@gmail.com>
2022-07-10 13:55:41 -0700

    test list

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../styles/global.css';
+</script>
+
+<div class="container mx-auto my-6 max-w-lg">
+	<slot />
+</div>
+
+
---
+++ src/routes/index.svelte
@@ -0,0 +1,43 @@
+<script>
+    let todos = [];
+    function addTodo() {
+        todos = [...todos, '']
+    }
+    function removeSelf(index) {
+todos = [...todos.slice(0, index), ...todos.slice(index+1)];
+    }
+</script>
+
+<div class="Title">
+	<h1>Exercise!</h1>
+</div>
+
+<div class = "list">
+    {#each todos as todos, index}
+        <input bind:value={todos[index]}>
+        <button on:click={()=>removeSelf(index)}>X</button>
+        <br>
+    {/each}
+<button on:click={addTodo}>Add</button>
+</div>
+
+
+
+<footer>
+    <h1>Get back into shape!</h1>
+</footer>
+
+<style>
+	.Title {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+    .list {
+        text-align: center;
+    }
+    footer{
+        text-align: center;
+    }
+    
+</style>
