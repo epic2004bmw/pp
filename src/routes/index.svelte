371fd20b4988423910a5ab09c4e3b72f45554538
Bryce Wong <epic2004bmw@gmail.com>
2022-07-25 14:36:25 -0700

    two

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,41 +1,77 @@
 <script>
-    let todos = [];
-    function addTodo() {
-        todos = [...todos, '']
-    }
-    function removeSelf(index) {
-todos = [...todos.slice(0, index), ...todos.slice(index+1)];
-    }
+	let todos = ['30 situps', '20 pushups', '5 pull ups'];
+	function addTodo() {
+		todos = [...todos, ''];
+	}
+	function removeSelf(index) {
+		todos = [...todos.slice(0, index), ...todos.slice(index + 1)];
+	}
+    let todosa = ['Dailies', '5 Bosses', 'Events', 'Clan Event', 'Extra Earnings'];
+	function addTodoa() {
+		todosa = [...todosa, ''];
+	}
+	function removeSelfa(index) {
+		todosa = [...todosa.slice(0, index), ...todosa.slice(index + 1)];
+	}
 </script>
 
 <div class="Title">
 	<h1>Exercise!</h1>
 </div>
 
-<div class = "list">
-    {#each todos as todos, index}
-        <input bind:value={todos[index]}>
-        <button on:click={()=>removeSelf(index)}>X</button>
-        <br>
-    {/each}
-<button on:click={addTodo}>Add</button>
+<div class="list">
+	{#each todos as todo, index}
+		<input bind:value={todos[index]} />
+		<button on:click={() => removeSelf(index)}>Done</button>
+		<br />
+	{/each}
+	<button on:click={addTodo}>Add</button>
 </div>
 
 <footer>
-    <h1>Get back into shape!</h1>
+	<h2>Get back into shape!</h2>
 </footer>
 
+<div class="Title">
+	<h1>Archero</h1>
+</div>
+
+<div class="list">
+	{#each todosa as todoa, index}
+		<input bind:value={todosa[index]} />
+		<button on:click={() => removeSelfa(index)}>Done</button>
+		<br />
+	{/each}
+	<button on:click={addTodoa}>Add</button>
+</div>
+
 <style>
+	.todos {
+		font-size: 44;
+	}
 	.Title {
 		text-align: center;
 		display: block;
 		margin: 20px auto;
+        font-size: large;
 	}
-    .list {
-        text-align: center;
-    }
-    footer{
-        text-align: center;
-    }
-    
+	.list {
+		text-align: center;
+		color: aqua;
+	}
+	button {
+		color: white;
+
+		background-color: red;
+		border-radius: 2px;
+		transition-duration: 0.4s;
+	}
+	button:hover {
+		background-color: red;
+		color: black;
+	}
+	footer {
+		text-align: center;
+        font-size: small;
+	}
 </style>
